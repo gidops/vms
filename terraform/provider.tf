77458667be0eb0ed935c52281b3d2c77bf7d1291
Gideon <ugorjigideon2@gmail.com>
2026-07-15 09:04:23 +0100

    fix: formated provider.tf file because it was failing CI

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = "~> 1.15"
 
   backend "s3" {
-    bucket         = "vms-tfstate-102969867136"
-    key            = "app/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true          # ← new: S3-native locking
-    encrypt        = true
+    bucket       = "vms-tfstate-102969867136"
+    key          = "app/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true # ← new: S3-native locking
+    encrypt      = true
   }
 
   required_providers {
